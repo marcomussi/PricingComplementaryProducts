5f7931fcd7f452afeb0b0e64bc0e0bd2011d00c2
Marco Mussi <marco.mussi@polimi.it>
2025-10-24 22:45:10 +0200

    Code before refractor for P prods=1

--- environments/complementary.py
+++ environments/complementary.py
@@ -5,13 +5,15 @@ import numpy as np
 class ComplementaryPricingEnvironment:
     
 
-    def __init__(self, n_products, n_actions, margins, demands, n_baskets, products_probs, alpha, graph_dict, mc_ep=1e3, seed=0):
+    def __init__(self, n_products, n_actions, margins, demands, n_baskets, products_probs, alpha, graph_dict, mc_ep=1000, seed=0):
         assert demands.shape == (n_products, n_actions, 2), "Shape of the demand not coherent"
         assert margins.shape == (n_products, n_actions), "Shape of the margins not coherent"
         assert products_probs.shape == (n_products, ), "Shape of the products_probs not coherent"
         assert (demands <= 1).all() and (demands >= 0).all(), "Error in demand values"
         assert (products_probs <= 1).all() and (products_probs >= 0).all(), "Error in demand values"
-        assert alpha >= 0 and alpha <= 1, "alpha must be in [0, 1]"        
+        assert alpha >= 0 and alpha <= 1, "alpha must be in [0, 1]"   
+        assert n_baskets >= 1, "n_baskets must be positive"  
+        assert mc_ep >= 10, "mc_ep too low for reliable estimates"   
         self.n_products = n_products
         self.n_actions = n_actions
         self.n_baskets = n_baskets
@@ -19,11 +21,11 @@ class ComplementaryPricingEnvironment:
         self.margins = margins
         self.alpha = alpha # 1 revenue, 0 profit
         self.products_probs = products_probs
-        self.graph_dict = graph_dict # elements for 0 to num_products - 1
+        self.graph_dict = graph_dict # elements for 0 to num_products-1
         self.mc_ep = mc_ep
         self.margins_to_idx_lst = []
         for prod in range(n_products):
-            self.margins_to_idx_lst.append({margins[prod, act]: act for act in range(n_actions)})
+            self.margins_to_idx_lst.append({self.margins[prod, idx]: idx for idx in range(0, self.n_actions)})
         self.leaders_lst = list(self.graph_dict.keys())
         self.followers_lst = list(self.graph_dict.values())
         aux = self.followers_lst.copy()
@@ -35,10 +37,10 @@ class ComplementaryPricingEnvironment:
         self.follower_to_leader_dict = {value: key for key, values in self.graph_dict.items() for value in values}
         for leader in self.leaders_lst:
                 followers = self.graph_dict[leader]
-                if isinstance(followers, list):
-                    for fl_i in range(1, len(followers)):
-                        assert (self.margins[followers[0], :] == self.margins[followers[fl_i], :]).all(), \
-                            "Followers of the same leader must have the same margins"
+                assert isinstance(followers, list), "All graph_dict values must be of type list"
+                for fl_i in range(1, len(followers)):
+                    assert (self.margins[followers[0], :] == self.margins[followers[fl_i], :]).all(), \
+                        "Followers of the same leader must have the same margins"
         self.action_values = self.compute_values()
         self.reset(seed)
 
@@ -46,25 +48,37 @@ class ComplementaryPricingEnvironment:
     def step(self, margins, override_n_baskets=None):
         assert margins.ndim == 1, "The action must be 1-dimensional"
         assert margins.shape[0] == self.n_products, "The action must be of dimension n_products"
+        
         if override_n_baskets is not None:
             n_bsk = override_n_baskets
         else:
             n_bsk = self.n_baskets
-        sales_impr_mx = np.zeros((self.n_products, n_bsk, 2), dtype=int) # 3rd dim: 0=sales, 1=impressions
+        sales_impr_mx = np.ones((self.n_products, n_bsk, 2), dtype=int) # 3rd dim: 0=sales, 1=impressions
+        
         sales_impr_mx[:, :, 1] = np.random.uniform(0, 1, (self.n_products, n_bsk)) < self.products_probs[:, np.newaxis]
+        
         for leader in self.leaders_lst:
-            demand = self.demands[leader, self.margins_to_idx_lst[leader][margins[leader]], 0] 
+            aux = self.margins_to_idx_lst[leader][margins[leader]]
+            demand = self.demands[leader, aux, 0] 
             sales_impr_mx[leader, :, 0] = np.random.uniform(0, 1, (n_bsk)) < demand
+        
         for follower in self.followers_lst:
+            
             corr_leader = self.follower_to_leader_dict[follower]
+            
             demand = self.demands[follower, self.margins_to_idx_lst[follower][margins[follower]], 0] 
             enhancement_demand = self.demands[follower, self.margins_to_idx_lst[follower][margins[follower]], 1]
-            mask = sales_impr_mx[corr_leader, :, 0] == 1
-            sales_demand = np.random.uniform(0, 1, (n_bsk - np.sum(mask))) < demand
-            sales_enhancement_demand = np.random.uniform(0, 1, (np.sum(mask))) < enhancement_demand 
-            sales_impr_mx[follower, np.logical_not(mask), 0] = sales_demand
-            sales_impr_mx[follower, mask, 0] = sales_enhancement_demand
+            
+            mask_leader_sales = sales_impr_mx[corr_leader, :, 0] == 1
+            
+            sales_demand = np.random.uniform(0, 1, (n_bsk - np.sum(mask_leader_sales))) < demand
+            sales_enhancement_demand = np.random.uniform(0, 1, (np.sum(mask_leader_sales))) < enhancement_demand
+
+            sales_impr_mx[follower, ~mask_leader_sales, 0] = sales_demand
+            sales_impr_mx[follower, mask_leader_sales, 0] = sales_enhancement_demand
+        
         sales_impr_mx[:, :, 0] = np.multiply(sales_impr_mx[:, :, 0], sales_impr_mx[:, :, 1])
+
         return sales_impr_mx
 
 
@@ -77,7 +91,7 @@ class ComplementaryPricingEnvironment:
                 vals = -1 * np.ones((self.n_actions, self.n_actions))
                 for leader_margin_i, leader_margin in enumerate(self.margins[leader, :]):
                     for followers_margin_i, followers_margin in enumerate(self.margins[followers[0], :]):
-                        margins_action = self.margins[:, 0]
+                        margins_action = self.margins[:, 0].copy() # ERRATO???
                         margins_action[leader] = leader_margin
                         for follower in followers:
                             margins_action[follower] = followers_margin
@@ -92,7 +106,7 @@ class ComplementaryPricingEnvironment:
             else:
                 vals = -1 * np.ones((self.n_actions, ))
                 for leader_margin_i, leader_margin in enumerate(self.margins[leader, :]):
-                    margins_action = self.margins[:, 0]
+                    margins_action = self.margins[:, 0].copy() # ERRATO???
                     margins_action[leader] = leader_margin
                     sales_impr_mx = self.step(margins_action, override_n_baskets=n_samples)
                     empirical_demand = np.sum(sales_impr_mx[leader, :, 0]) / n_samples
@@ -125,108 +139,3 @@ class ComplementaryPricingEnvironment:
 
     def reset(self, seed=0):
         np.random.seed(seed)
-
-
-
-"""
-class ComplementaryPricingEnvOLD:
-
-    
-    def __init__(self, n_products, n_actions, actions, demands, costs, user_range, 
-                 graph_dict, compl_factor_add, compl_factor_molt, mc_ep, seed):
-        assert demands.shape == (n_products, n_actions), "Shape of the demand not coherent"
-        assert actions.shape == (n_actions, ), "Shape of the actions not coherent"
-        assert (demands <= 1).all() and (demands >= 0).all(), "Error in demand values"
-        assert user_range.shape == (n_products, 2), "Shape of user_range not coherent"
-        assert (user_range > 0).all(), "user_range must be strictly greater than zero"
-        assert (user_range[:, 0] <= user_range[:, 1]).all(), "user_range not coherent"
-        assert compl_factor_molt >= 1.0, "compl_factor_molt not coherent"
-        assert compl_factor_add >= 0.0, "compl_factor_add not coherent"
-        self.n_products = n_products
-        self.n_actions = n_actions
-        self.demands = demands
-        self.actions = actions
-        self.costs = costs
-        self.user_range = user_range
-        self.graph_dict = graph_dict
-        self.compl_factor_add = compl_factor_add
-        self.compl_factor_molt = compl_factor_molt
-        self.mc_ep = mc_ep
-        self.max_users = np.max(user_range)
-        actions0, actions1 = np.meshgrid(self.actions, self.actions)
-        self.byvariate_actions = np.hstack((actions0.ravel().reshape(-1, 1), actions1.ravel().reshape(-1, 1)))
-        actions0, actions1 = np.meshgrid(np.linspace(0, self.n_actions-1, self.n_actions, dtype=int), 
-                                         np.linspace(0, self.n_actions-1, self.n_actions, dtype=int))
-        self.byvariate_actions_map = np.hstack((actions0.ravel().reshape(-1, 1), actions1.ravel().reshape(-1, 1)))
-        np.random.seed(seed)
-
-    
-    def reset(self, seed=0):
-        np.random.seed(seed)
-
-    
-    def step(self, action):
-        assert action.ndim == 1, "The action must be 1-dimensional"
-        assert action.shape[0] == self.n_products, "The action must be of dimension n_products"
-        demand_acts = np.array([self.demands[i, action[i]] for i in range(self.n_products)])
-        cumul_mx = np.zeros((self.n_products, 2), dtype=int)
-        cumul_mx[:, 1] = (np.random.uniform(0, 1, self.n_products) * (self.user_range[:, 1] - self.user_range[:, 0]
-                                                                  ) + self.user_range[:, 0]).astype(np.int64)
-        cumul_mx[:, 0] = np.random.binomial(cumul_mx[:, 1], demand_acts)
-        impressions_mx = np.zeros((self.n_products, self.max_users))
-        sales_mx = np.zeros((self.n_products, self.max_users))
-        for key in list(self.graph_dict.keys()):
-            idx_impr = np.random.choice(self.max_users, cumul_mx[key, 1], replace=False)
-            impressions_mx[key, idx_impr] = 1
-            idx_sales = np.random.choice(idx_impr, cumul_mx[key, 0], replace=False)
-            sales_mx[key, idx_sales] = 1
-            for elem in self.graph_dict[key]:
-                idx_impr = np.random.choice(self.max_users, cumul_mx[elem, 1], replace=False)
-                impressions_mx[elem, idx_impr] = 1
-                for idx in list(idx_impr):
-                    dem = demand_acts[elem]
-                    if impressions_mx[key, idx] == 1:
-                        dem = dem * self.compl_factor_molt + self.compl_factor_add
-                    sales_mx[elem, idx] = np.random.binomial(1, dem)
-                cumul_mx[elem, 0] = np.sum(sales_mx[elem, :])
-        return cumul_mx, impressions_mx, sales_mx
-    
-
-    def compute_optimum(self):
-        optimal_actions = -1 * np.ones(self.n_products, dtype=int)
-        optimal_value = 0
-        for key in list(self.graph_dict.keys()):
-            map_idx, value = self._montecarlo_simulation(key, self.graph_dict[key])
-            optimal_value += value
-            optimal_actions[key] = self.byvariate_actions_map[map_idx, 0]
-            for fol in self.graph_dict[key]:
-                optimal_actions[fol] = self.byvariate_actions_map[map_idx, 1]
-        return optimal_actions, optimal_value
-
-    
-    def compute_optimum_indep(self):
-        optimal_actions = -1 * np.ones(self.n_products, dtype=int)
-        optimal_value = 0
-        for key in list(np.linspace(0, self.n_products - 1, self.n_products, dtype=int)):
-            map_idx, value = self._montecarlo_simulation(key, [])
-            optimal_value += value
-            optimal_actions[key] = self.byvariate_actions_map[map_idx, 0]
-        return optimal_actions, optimal_value
-
-    
-    def _montecarlo_simulation(self, leader, follower_lst):
-        obj = np.zeros(self.byvariate_actions.shape[0])
-        for i in range(self.byvariate_actions.shape[0]):
-            sales_tot = np.zeros(self.n_products)
-            action_vect = np.zeros(self.n_products, dtype=int)
-            action_vect[leader] = self.byvariate_actions_map[i, 0]
-            for j in follower_lst:
-                action_vect[j] = self.byvariate_actions_map[i, 1]
-            for sim in range(self.mc_ep):
-                sales, _, _ = self.step(action_vect)
-                sales_tot = sales_tot + sales[:, 0]
-            obj[i] = obj[i] + self.costs[leader] * sales_tot[leader] * self.byvariate_actions[i, 0]
-            for fol in follower_lst: 
-                obj[i] = obj[i] + self.costs[fol] * sales_tot[fol] * self.byvariate_actions[i, 1]
-        return np.argmax(obj), np.max(obj) / self.mc_ep
-"""
